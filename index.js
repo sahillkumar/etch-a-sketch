9557ef87e3537074bcd616d3cd5d3b21152c1302
Sahil Kumar <sahil511kumar@gmail.com>
2024-06-19 23:07:39 +0530

    added interactions and updated styles

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
-let rows = 5;
-let cols = 5;
+let rows;
+let cols;
 let imgIndex = 0;
+let mode = "solid";
+let shade;
 
 const sketchContainer = document.querySelector(".sketchContainer");
 const sketch = document.createElement("div");
 const rowSelector = document.querySelector("#rows");
 const colSelector = document.querySelector("#cols");
-const modeSelector = document.querySelector("#modeSelector");
 const currentColor = document.querySelector("#currentColor");
 const shuffleBtn = document.querySelector(".shuffleBtn");
 const resetBtn = document.querySelector(".resetBtn");
 const shuffleAndResetBtn = document.querySelector(".shuffleAndResetBtn");
+const radioGroup = document.querySelector(".radioGroup");
+const solidMode = document.getElementById("solid");
+const rowCount = document.getElementById("rowCount");
+const colCount = document.getElementById("colCount");
+
+const afterEffects = (mode) => {
+  const disabled = mode === "rainbow" ? true : false;
+  currentColor.disabled = disabled;
+  currentColor.classList.toggle("disabled");
+};
+
+currentColor.addEventListener("change", (e) => {
+  shade = e.target.value;
+});
+
+radioGroup.onclick = (e) => {
+  if (e.target.name === "mode") {
+    mode = e.target.value;
+    afterEffects(mode);
+  }
+};
 
 rowSelector.onchange = (e) => {
   rows = e.target.value;
+  rowCount.textContent = rows;
   createGrid(rows, cols);
 };
 
 colSelector.onchange = (e) => {
   cols = e.target.value;
+  colCount.textContent = cols;
   createGrid(rows, cols);
 };
 
@@ -60,28 +84,44 @@ function createGrid() {
   sketchContainer.appendChild(sketch);
 }
 
+const getRandomColor = () => {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
 const changeColor = (e) => {
-  e.target.style.backgroundColor = "white";
+  const currShade = mode === "rainbow" ? getRandomColor() : shade;
+  e.target.style.backgroundColor = currShade;
 };
 
 window.onload = () => {
-  createGrid(rows, cols);
   sketch.style.backgroundImage = `url(${photos[imgIndex]})`;
+  solidMode.checked = true;
+  shade = currentColor.value;
+
+  rows = rowSelector.value;
+  cols = colSelector.value;
+  colCount.textContent = cols;
+  rowCount.textContent = rows;
+  createGrid(rows, cols);
 };
 
 const photos = [
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaeUHfGpWLPe6qoloDdkEIMTukNh0YH6FqNw&s",
-  "https://images.livemint.com/img/2024/03/25/1140x641/pexels-jack-geoghegan-1044056_1711197278952_1711334456910.jpg",
-  "https://images.pexels.com/photos/709552/pexels-photo-709552.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-  "https://captainfi.com/wp-content/uploads/2022/07/Pexels-free-images-3-1024x646.jpg",
-  "https://images.pexels.com/photos/1996333/pexels-photo-1996333.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-  "https://www.hindustantimes.com/ht-img/img/2024/04/22/550x309/best_skybags_luggage_1713789326726_1713789338779.jpg",
-  "https://ianslife.in/sites/default/files/styles/detail_page/public/2024-05/pexels-olly-3781538.jpg?itok=SWsFmblX",
-  "https://images.pexels.com/photos/414144/pexels-photo-414144.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-  "https://bsmedia.business-standard.com/_media/bs/img/article/2023-04/13/full/1681383051-827.jpg?im=FeatureCrop,size=(826,465)",
-  "https://images.pexels.com/photos/3278215/pexels-photo-3278215.jpeg?cs=srgb&dl=pexels-freestockpro-3278215.jpg&fm=jpg",
-  "https://images.pexels.com/photos/315998/pexels-photo-315998.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-  "https://images.indianexpress.com/2022/03/Skincare-Pexels-3.jpg",
-  "https://images.unsplash.com/photo-1505820013142-f86a3439c5b2?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.pexels.com/photos/355465/pexels-photo-355465.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  "https://resizing.flixster.com/IqpM_IQdzeGrQuBZNc-W3J0h2s0=/fit-in/705x460/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p10399541_i_h9_aa.jpg",
+  "https://www.desktopbackground.org/p/2011/03/02/165718_zootopia-wallpaper-movies-animation-zootopia-best-animation_4767x3300_h.jpg",
+  "https://i.pinimg.com/736x/68/f1/71/68f171a7671807151e8c7615043e4e57.jpg",
+  "https://a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2018/09/931/524/1c1efe99-the-simpsons-660-ap.jpg?ve=1&tl=1",
+  "https://static1.srcdn.com/wordpress/wp-content/uploads/2016/12/Scooby-Doo-and-the-Mystery-Gang.jpg",
+  "https://rukminim2.flixcart.com/image/850/1000/kkwwu4w0/poster/x/p/p/medium-doraemon-cartoon-waterproof-vinyl-sticker-poster-can2481-original-imagy5emxzpffnwm.jpeg?q=90&crop=false",
+  "https://w0.peakpx.com/wallpaper/247/434/HD-wallpaper-anime-girl-cute-cartoon-characters.jpg",
+  "https://www.boredpanda.com/blog/wp-content/uploads/2022/02/cute-animated-characters-17-6206579c2dad1__700.jpg",
+  "https://www.moople.in/blog/wp-content/uploads/2018/02/mickey-mouse-30-2024.jpg",
+  "https://variety.com/wp-content/uploads/2016/05/adventure-time.jpg?w=1000&h=667&crop=1",
+  "https://images.wondershare.com/filmora/article-images/2022/04/iconic%20cartoon%20characters-4.jpg",
+  "https://cdn.britannica.com/26/175326-050-8E3D0CF1/scene-Dumbo.jpg",
+  "https://sm.ign.com/ign_in/screenshot/default/dragon-ball-super-goku-image-2_29aa.jpg",
 ];
